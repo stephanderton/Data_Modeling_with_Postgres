3f279f120dbc16a84b13184bef8fbdc7cfef38aa
Stephanie Anderton <steph.anderton@gmail.com>
2019-04-15 14:35:21 -0400

    Updated CREATE and INSERT statements.
    Managed conflicts for INSERT. Specified PRIMARY KEYS, NOT NULLs. Modified data types for some features.

--- sql_queries.py
+++ sql_queries.py
@@ -6,15 +6,16 @@ song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
+
 # CREATE TABLES
 
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (  \
-        songplay_id INT,  \
-        start_time BIGINT,  \
-        user_id INT,  \
-        level VARCHAR,  \
-        song_id VARCHAR,  \
-        artist_id INT,  \
+        songplay_id SERIAL PRIMARY KEY,  \
+        start_time BIGINT NOT NULL,  \
+        user_id INT NOT NULL,  \
+        level VARCHAR NOT NULL,  \
+        song_id VARCHAR NOT NULL,  \
+        artist_id VARCHAR NOT NULL,  \
         session_id INT,  \
         location VARCHAR,  \
         user_agent VARCHAR  \
@@ -22,26 +23,26 @@ songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (  \
 """)
 
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users (  \
-        user_id INT,  \
-        first_name VARCHAR,  \
-        last_name VARCHAR,  \
+        user_id INT PRIMARY KEY,  \
+        first_name VARCHAR NOT NULL,  \
+        last_name VARCHAR NOT NULL,  \
         gender VARCHAR,  \
-        level VARCHAR  \
+        level VARCHAR NOT NULL  \
     );  \
 """)
 
 song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (  \
-        song_id VARCHAR,  \
-        title VARCHAR,  \
-        artist_id VARCHAR,  \
+        song_id VARCHAR PRIMARY KEY,  \
+        title VARCHAR NOT NULL,  \
+        artist_id VARCHAR  NOT NULL,  \
         year INT,  \
         duration REAL  \
     );  \
 """)
 
 artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (  \
-        artist_id VARCHAR,  \
-        name VARCHAR,  \
+        artist_id VARCHAR PRIMARY KEY,  \
+        name VARCHAR NOT NULL,  \
         location VARCHAR,  \
         latitude DOUBLE PRECISION,  \
         longitude DOUBLE PRECISION  \
@@ -49,55 +50,72 @@ artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (  \
 """)
 
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time (  \
-        start_time BIGINT,  \
-        hour INT,  \
-        day INT,  \
-        week INT,  \
-        month INT,  \
-        year INT,  \
-        weekday INT  \
+        start_time BIGINT PRIMARY KEY,  \
+        hour SMALLINT NOT NULL,  \
+        day SMALLINT NOT NULL,  \
+        week SMALLINT NOT NULL,  \
+        month SMALLINT NOT NULL,  \
+        year SMALLINT NOT NULL,  \
+        weekday SMALLINT NOT NULL  \
     );  \
 """)
+
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""INSERT INTO songplays (  \
         songplay_id, start_time, user_id, level, song_id, artist_id,  \
         session_id, location, user_agent  \
-        )  \
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)  \
+    )  \
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)  \
+    ON CONFLICT (ts, user_id)  \
+    DO UPDATE  \
+        SET ts = EXCLUDED.ts, user_id = EXCLUDED.user_id  \
 """)
 
 user_table_insert = ("""INSERT INTO users (  \
         user_id, first_name, last_name, gender, level  \
-        )  \
-        VALUES (%s, %s, %s, %s, %s)  \
+    )  \
+    VALUES (%s, %s, %s, %s, %s)  \
+    ON CONFLICT (user_id)  \
+    DO UPDATE  \
+        SET user_id = EXCLUDED.user_id  \
 """)
 
 song_table_insert = ("""INSERT INTO songs (  \
         song_id, title, artist_id, year, duration  \
-        )  \
-        VALUES (%s, %s, %s, %s, %s)  \
+    )  \
+    VALUES (%s, %s, %s, %s, %s)  \
+    ON CONFLICT (song_id)  \
+    DO UPDATE  \
+        SET song_id = EXCLUDED.song_id  \
 """)
 
 artist_table_insert = ("""INSERT INTO artists (  \
         artist_id, name, location, latitude, longitude  \
-        )  \
-        VALUES (%s, %s, %s, %s, %s)  \
+    )  \
+    VALUES (%s, %s, %s, %s, %s)  \
+    ON CONFLICT (artist_id)  \
+    DO UPDATE  \
+        SET artist_id = EXCLUDED.artist_id  \
 """)
-
 
 time_table_insert = ("""INSERT INTO time (  \
         start_time, hour, day, week, month, year, weekday  \
-        )  \
-        VALUES (%s, %s, %s, %s, %s, %s, %s)  \
+    )  \
+    VALUES (%s, %s, %s, %s, %s, %s, %s)  \
+    ON CONFLICT (start_time)  \
+    DO UPDATE  \
+        SET start_time = EXCLUDED.start_time  \
 """)
+
 
 # FIND SONGS
 
 song_select = ("""SELECT * FROM songs   \
-        WHERE song_id = 'SOMVWWT12A58A7AE05'  \
+        WHERE song_id = 'SOVYKGO12AB0187199'  \
 """)
+
 
 # QUERY LISTS
 
