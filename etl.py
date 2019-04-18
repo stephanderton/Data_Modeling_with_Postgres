9132dbe0764eb511d1fbb2fac50251f1f5b10226
Stephanie Anderton <steph.anderton@gmail.com>
2019-04-18 01:01:04 -0400

    Completed ETL pipeline code.
    Cleaned up the Jupyter notebook and migrated the code over to etl.py. ETL processed song_data and log_data files.

--- etl.py
+++ etl.py
@@ -2,42 +2,69 @@ import os
 import glob
 import psycopg2
 import pandas as pd
+import json
 from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """
+
+    """
+
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
+
+    # substitute apostrophe with escaped char to avoid failure on INSERT
+    df['artist_location'] = df['artist_location'].str.replace("'", "\'")
+    df['artist_name'] = df['artist_name'].str.replace("'", "\'")
+    df['title'] = df['title'].str.replace("'", "\'")
 
     # insert song record
-    song_data = 
+    song_data = df[['song_id', 'title', 'artist_id', 'year', 
+                    'duration']].values[0].tolist()
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = df[['artist_id', 'artist_name', 
+                      'artist_location', 'artist_latitude', 
+                      'artist_longitude']].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
+    """
+
+    """
+
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df.page == 'NextSong']
+
+    # substitute apostrophe with escaped char to avoid failure on INSERT
+    df['artist'] = df['artist'].str.replace("'", "\'")
+    df['firstName'] = df['firstName'].str.replace("'", "\'")
+    df['lastName'] = df['lastName'].str.replace("'", "\'")
+    df['location'] = df['location'].str.replace("'", "\'")
+    df['song'] = df['song'].str.replace("'", "\'")
+    df['userAgent'] = df['userAgent'].str.replace("'", "\'")
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df['ts'], unit='ms')
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = [df.ts, t.dt.hour, t.dt.day, t.dt.week, t.dt.month, 
+                 t.dt.year, t.dt.weekday]
+    column_labels = ('start_time', 'hour', 'day', 'week', 'month', 
+                     'year', 'weekday')
+    time_df = pd.DataFrame.from_dict(dict(zip(column_labels, time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -47,20 +74,20 @@ def process_log_file(cur, filepath):
     for index, row in df.iterrows():
         
         # get songid and artistid from song and artist tables
-        cur.execute(song_select, (row.song, row.artist, row.length))
-        results = cur.fetchone()
-        
-        if results:
-            songid, artistid = results
-        else:
-            songid, artistid = None, None
+        results = cur.execute(song_select, (row.song, row.artist, row.length))
+        songid, artistid = results if results else None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (row.ts, row.userId, row.level, songid, artistid,
+                         row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
+    """
+
+    """
+
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -80,13 +107,22 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
-    cur = conn.cursor()
+    """
 
-    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    """
 
-    conn.close()
+    try:
+        conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+        cur = conn.cursor()
+        print("Connected the sparkifydb database")
+    except psycopg2.Error as e: 
+        print("Error: Could not connect to the Database")
+        print(e)
+    else:
+        process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+        process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+
+        conn.close()
 
 
 if __name__ == "__main__":
